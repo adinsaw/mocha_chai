5d544a6ede376eed49c4b140d96bca3a38e429ae
adinsaw <adinsaw@gmail.com>
2016-06-16 14:20:57 -0500

    Update test1.spec.js

--- app/test/test1.spec.js
+++ app/test/test1.spec.js
@@ -1,5 +1,4 @@
 "use strict";
-
 var testing_1 = require('@angular/core/testing');
 testing_1.describe('1st tests', function () {
     testing_1.it('true is true', function () { return expect(true).toEqual(true); });
