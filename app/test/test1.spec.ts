8edcb15f748304799bdb58081dbab571ba58b704
adinsaw <adinsaw@gmail.com>
2016-06-17 10:22:15 -0500

    Update test1.spec.ts

--- app/test/test1.spec.ts
+++ app/test/test1.spec.ts
@@ -2,9 +2,6 @@
 
     import { describe,it } from '@angular/core/testing';
 
-describe('1st tests', () => {
-      it('true is true', () => expect(true).toEqual(true));
-    });
      
      describe('1st tests', () => {
          it('true is false', () => expect(true).toEqual(true));
